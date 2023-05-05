d60746e47b544cfa9e8965ca91f5c189fa80e343
Luca Castellaro <luca_castellaro@outlook.com>
2023-05-05 11:56:27 +0200

    LC - Add tests

---
+++ ProvaGithubActions.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ProvaGithubActions.Tests;
+
+public sealed class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(1 + 1 == 2);
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        Assert.False(1 + 1 == 3);
+    }
+}
